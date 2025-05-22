e1257cc6a0e0403d96dd34377efb383f90a993a3
Jacob McCollum <jacob.r.mccollum@gmail.com>
2025-05-21 23:35:29 -0400

    some basics

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,22 +1,92 @@
-import "./style/main.scss";
+import "./style/index.scss";
 
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { JSX, render } from "preact";
+import { useEffect } from "preact/hooks";
+import { ReadonlySignal, Signal, useComputed, useSignal, useSignalEffect } from "@preact/signals";
+import { computePosition } from "@floating-ui/dom";
 
-import { render } from "preact";
+const PopUpMenu = ({
+  position,
+  active,
+}: {
+  position: ReadonlySignal<{ clientX: number; clientY: number }>;
+  active: Signal<boolean>;
+}) => {
+  useEffect(() => {
+    let seenOnce = false;
+    const unsubscribe = active.subscribe(() => (seenOnce = false));
 
-import { AppStateProvider } from "./state/AppState";
-import { App } from "./components/App";
+    const deactivateMenu = () => {
+      if (active.value) {
+        if (seenOnce) {
+          active.value = false;
+        } else {
+          seenOnce = true;
+        }
+      }
+    };
+    window.addEventListener("mouseup", deactivateMenu);
+
+    return () => {
+      unsubscribe();
+      window.removeEventListener("mouseup", deactivateMenu);
+    };
+  }, []);
+
+  const menuStyle = useComputed<Partial<JSX.CSSProperties>>(() => ({
+    display: active.value ? "block" : "none",
+    position: "fixed",
+    left: position.value.clientX,
+    top: position.value.clientY,
+  }));
+
+  return (
+    <ul class="popup-menu" style={menuStyle.value}>
+      <li title="some info" onClick={() => console.log("woohoo1")}>
+        Create Point
+      </li>
+      <li title="some info 2" onClick={() => console.log("woohoo2")}>
+        Remove Point
+      </li>
+      <li title="some info 3" onClick={() => console.log("woohoo3")}>
+        Modify Point
+      </li>
+    </ul>
+  );
+};
 
 const Main = () => {
+  const menuActive = useSignal(false);
+  const menuPosition = useSignal({ clientX: 0, clientY: 0 });
+
+  useEffect(() => {
+    window.addEventListener("contextmenu", (evt) => {
+      evt.preventDefault();
+      menuActive.value = !menuActive.value;
+      menuPosition.value = { clientX: evt.clientX, clientY: evt.clientY };
+    });
+  }, []);
+
   return (
-    <AppStateProvider>
-      <App />
-    </AppStateProvider>
+    <>
+      <button class="primary">primary</button>
+      <button class="secondary">secondary</button>
+      <button class="warning">warning</button>
+      <button class="danger">danger</button>
+      <button class="success">success</button>
+      <button class="info">info</button>
+      <button class="dark">dark</button>
+      <button class="light">light</button>
+      <button class="clear">clear</button>
+
+      <button class="primary rounded">primary rounded</button>
+      <button class="secondary rounded">secondary rounded</button>
+      <button class="success rounded">&nbsp;&nbsp;&nbsp;</button>
+      <PopUpMenu position={menuPosition} active={menuActive} />
+    </>
   );
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-  document.body.classList.add("vectim8");
-
   render(<Main />, document.body);
 });
